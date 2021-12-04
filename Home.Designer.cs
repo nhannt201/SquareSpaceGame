33a375d0c85326d10690db7975e513247055e7fa
nhannt201 <trungnhan21.12@gmail.com>
2021-12-04 19:44:58 +0700

    U

---
+++ Home.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace NoelGame
+{
+    partial class Home
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.users = new System.Windows.Forms.PictureBox();
+            this.rock = new System.Windows.Forms.PictureBox();
+            this.thong1 = new System.Windows.Forms.PictureBox();
+            this.thong2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.users)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thong1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thong2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // users
+            // 
+            this.users.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.users.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.users.Location = new System.Drawing.Point(348, 394);
+            this.users.Name = "users";
+            this.users.Size = new System.Drawing.Size(158, 212);
+            this.users.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.users.TabIndex = 0;
+            this.users.TabStop = false;
+            // 
+            // rock
+            // 
+            this.rock.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.rock.Location = new System.Drawing.Point(1537, 444);
+            this.rock.Name = "rock";
+            this.rock.Size = new System.Drawing.Size(146, 213);
+            this.rock.TabIndex = 1;
+            this.rock.TabStop = false;
+            // 
+            // thong1
+            // 
+            this.thong1.BackColor = System.Drawing.Color.Green;
+            this.thong1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.thong1.Location = new System.Drawing.Point(1547, 311);
+            this.thong1.Name = "thong1";
+            this.thong1.Size = new System.Drawing.Size(180, 205);
+            this.thong1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.thong1.TabIndex = 2;
+            this.thong1.TabStop = false;
+            // 
+            // thong2
+            // 
+            this.thong2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.thong2.Location = new System.Drawing.Point(1772, 206);
+            this.thong2.Name = "thong2";
+            this.thong2.Size = new System.Drawing.Size(241, 310);
+            this.thong2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.thong2.TabIndex = 3;
+            this.thong2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.MintCream;
+            this.pictureBox1.Location = new System.Drawing.Point(-6, -5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1889, 470);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbScore
+            // 
+            this.lbScore.AutoSize = true;
+            this.lbScore.BackColor = System.Drawing.Color.MintCream;
+            this.lbScore.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbScore.Location = new System.Drawing.Point(28, 22);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(189, 60);
+            this.lbScore.TabIndex = 5;
+            this.lbScore.Text = "Score: 0";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1464, 829);
+            this.Controls.Add(this.lbScore);
+            this.Controls.Add(this.users);
+            this.Controls.Add(this.rock);
+            this.Controls.Add(this.thong1);
+            this.Controls.Add(this.thong2);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Square Game";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Home_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.users)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thong1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thong2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox users;
+        private System.Windows.Forms.PictureBox rock;
+        private System.Windows.Forms.PictureBox thong1;
+        private System.Windows.Forms.PictureBox thong2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbScore;
+    }
+}
---
+++ Home.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoelGame
+{
+    public partial class Home : Form
+    {
+        static int user_x_default = 0;
+        static int user_y_default = 0;
+
+        static int rockX = 0;
+        static int rockY = 0;
+
+        static int thong1X = 0;
+        static int thong1Y = 0;
+
+        static int thong2X = 0;
+        static int thong2Y = 0;
+
+        static int score = 0;
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            user_x_default = users.Location.X;
+            user_y_default= users.Location.Y;
+
+            rockX = rock.Location.X;
+            rockY = rock.Location.Y;
+
+            thong1X = thong1.Location.X;
+            thong1Y = thong1.Location.Y;
+
+            thong2X = thong2.Location.X;
+            thong2Y = thong2.Location.Y;
+
+
+
+            rockLeft();
+
+            bgTree();
+
+        }
+
+        private bool CheckRockTouch()
+        {
+
+            return (users.Location.Y + users.Height > rock.Location.Y) && (users.Location.Y < rock.Location.Y + rock.Height) && (rock.Location.X + rock.Width > users.Location.X) && (rock.Location.X < users.Location.X + users.Width);
+
+        }
+
+      //  bool checkRockX()
+     //   {
+        //    return (rock.Location.X + rock.Width > users.Location.X) && (rock.Location.X < users.Location.X + users.Width);
+       // }
+
+
+
+        static int xUp = 0;
+        async void spaceUp()
+        {
+            await Task.Delay(10);
+            xUp += 1;
+            users.Location = new Point(user_x_default, users.Location.Y - 20);
+            users.Refresh(); 
+            if (xUp<20) { spaceUp(); }
+            else { spaceDown(); }
+            
+        }
+
+        async void spaceDown()
+        {
+            await Task.Delay(10);
+            xUp -= 1;
+            users.Location = new Point(user_x_default, users.Location.Y + 20);
+            users.Refresh();
+            if (xUp > 0) { spaceDown(); } else {
+                users.Location = new Point(user_x_default, user_y_default);
+                users.Refresh();
+            }
+        }
+        private void Home_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ')
+            {
+                spaceUp();
+                realeaseMemory();
+            }
+
+        }
+
+        async void rockLeft()
+        {
+                await Task.Delay(10);
+
+            rock.Left -= 20;
+           if(CheckRockTouch())
+            {
+                MessageBox.Show("Game lost!");
+                realeaseMemory();
+                rock.Location = new Point(rockX, rockY);
+                rock.Refresh();
+                score = 0;
+                lbScore.Text = "Score: " + score.ToString();
+                rockLeft();
+            } else
+            {
+                if (rock.Location.X + rock.Width < 0)
+                {
+                    rock.Location = new Point(rockX, rockY);
+                    rock.Refresh();
+                    score += 1;
+                    if (score >= 50)
+                    {
+                        MessageBox.Show("You win!");
+                        score = 0;
+                    }
+                    lbScore.Text = "Score: " + score.ToString();
+                    rockLeft();
+                }
+                else
+                {
+                    rockLeft();
+                }
+
+
+            }
+           
+        }
+
+        async void bgTree()
+        {
+            if (score < 10)
+            {
+                await Task.Delay(50);
+            }
+            else if (score < 20)
+            {
+                await Task.Delay(40);
+            }
+            else if (score < 30)
+            {
+                await Task.Delay(30);
+            }
+            else if (score < 40)
+            {
+                await Task.Delay(20);
+            }
+            //Cay thong1
+            thong1.Left -= 10;
+            if (thong1.Location.X + thong1.Width < 0)
+            {
+                thong1.Location = new Point(thong1X, thong1Y);
+                thong1.Refresh();
+            } 
+            //Caythong2
+            thong2.Left -= 10;
+            if (thong2.Location.X + thong2.Width < 0)
+            {
+                thong2.Location = new Point(thong2X, thong2Y);
+                thong2.Refresh();
+            }
+            bgTree();
+        }
+
+
+            private static void realeaseMemory()
+        {
+
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+
+
+
+    }
+}
